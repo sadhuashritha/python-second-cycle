a8b8f1766e8ffc348043b4da2f7b5d4464c05148
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-03-29 12:14:28 +0530

    updated

--- functions.py
+++ functions.py
@@ -46,52 +46,9 @@ def eo(n):
 eo(9)
 print()
 
-# Write a function to find the factorial of a number.
-def facto(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * facto(n - 1)
-print(facto(3))
-print()
 
-# Write a function that returns the maximum of three numbers.
-def maxi(*n):
-    great = 0
-    for i in n:
-        if i >= great:
-            great = i
-    return great
-print(maxi(5,6,980))
-print()
-
-# Write a function to count vowels in a string.
-def vowel(n):
-    count = 0
-    for i in n:
-        if i in "aeiouAEIOU":
-            count += 1
-    return count
-print(vowel("ashritha"))
-print()
-# Write a function that returns multiple values (sum and product of two numbers).
-def sm(*n):
-    sum= 0
-    prod = 1
-    for i in n:
-        sum += i
-        prod *= i
-    return (sum,prod)
-print(sm(1,2,1))
-print()
-
-# Write a function that checks if a string is palindrome.
-def palin(n):
-    if n == n[::-1]:
-        return True
-    else:
-        return False
-print(palin("wow"))
 # Write a function to find GCD of two numbers.
+
 # Write a function to return list of prime numbers up to n.
+
 # Write a function that takes a list and returns only unique elements.
