1d0435f33f542fcfef55e24707a6242cd6cb1c98
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-03-31 23:40:54 +0530

    arguments file updated

---
+++ oops.py
@@ -0,0 +1,29 @@
+'''
+class Employee:
+    def empmethod(self):
+        print("Employee parent class and method")
+
+class Developer(Employee):
+    def devmethod(self):
+        print("Developer child class and method")
+        
+ed = Developer()
+ed.empmethod()
+ed.devmethod()
+
+'''
+'''
+1. Single Inheritance (Basic)
+
+Create a class Animal with a method eat().
+Create a class Dog that inherits from Animal and calls eat().
+
+class Animal:
+    def eat(self):
+        print("Animal (parent)class")
+class Dog(Animal):
+    print("Animal")
+d = Dog()
+d.eat()
+
+'''
