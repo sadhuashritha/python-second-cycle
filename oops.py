a9bc2ba31cbab85265036b96acd3fa4bd7e0db2d
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-04-02 21:00:00 +0530

    Solved LeetCode problems

--- oops.py
+++ oops.py
@@ -1,4 +1,3 @@
-'''
 class Employee:
     def empmethod(self):
         print("Employee parent class and method")
@@ -11,20 +10,4 @@ ed = Developer()
 ed.empmethod()
 ed.devmethod()
 
-'''
 
-'''
-1. Single Inheritance (Basic)
-
-Create a class Animal with a method eat().
-Create a class Dog that inherits from Animal and calls eat().
-
-class Animal:
-    def eat(self):
-        print("Animal (parent)class")
-class Dog(Animal):
-    print("Animal")
-d = Dog()
-d.eat()
-
-'''
