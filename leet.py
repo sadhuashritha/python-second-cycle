b68034183e16ced54b4a5edf93da618b4776a465
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-04-04 21:57:57 +0530

    updated a file

--- leet.py
+++ leet.py
@@ -8,19 +8,3 @@ class Solution:
 
 
            
-# 1880. Check if Word Equals Summation of Two Words
-class Solution:
-    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
-        val = ""
-        for i in firstWord:
-            val += str(ord(i) - ord('a'))
-
-        val1 = ""
-        for j in secondWord:
-            val1 += str(ord(j) - ord('a'))
-        
-        value = ""
-        for k in targetWord:
-            value += str(ord(k) - ord('a'))
-        
-        return int(val) + int(val1) == int(value)
