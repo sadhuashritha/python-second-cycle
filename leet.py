6c85b96da61795816ea9fa1e00e709a7f5660d95
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-04-05 21:00:00 +0530

    Solved LeetCode problems

--- leet.py
+++ leet.py
@@ -25,18 +25,3 @@ class Solution:
         
         return int(val) + int(val1) == int(value)
 
-
-#initialise a parent class as Employee-constructor with parameter name, child class as Developer - constructor with parameter prog_lang, user super()
-
-class Employee:
-    def __init__(self,name):
-        self.name = name
-
-class Developer(Employee):
-    def __init__(self,name,fullname):
-        super().__init__(name)
-        self.fullname = fullname
-
-d = Developer("Ashritha","Sadhu")
-print(d.name)
-print(d.fullname)
