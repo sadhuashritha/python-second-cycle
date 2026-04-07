8dfea9b06b2b0366eb481388ed916b08e4b4841e
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-04-07 23:01:45 +0530

    added a file

--- oops.py
+++ oops.py
@@ -23,3 +23,49 @@ class Dog(Animal):
     print("Animal")
 d = Dog()
 d.eat()
+
+
+'''
+Create an Abstract Class
+Create an abstract class Shape with:
+
+abstract method area()
+abstract method perimeter()
+
+Then create:
+
+Circle
+Rectangle
+'''
+from abc import ABC, abstractmethod
+class shape(ABC):
+    @abstractmethod
+    def area(self):
+        print("Area method")
+    def perimeter(self):
+        print("Perimeter method")
+        
+class Circle(shape):
+    def area(self):
+        print("Circle area")
+    def perimeter(self):
+        print("Circle perimeter")
+    def total(self):
+        print("Total")
+        
+class Rectangle(shape):
+    def area(self):
+        print("Rectangle area")
+    def perimeter(self):
+        print("Rectangle perimeter")
+    def total(self):
+        print("Total")
+
+c = Circle()
+r = Rectangle()
+
+c.area()
+c.perimeter()
+
+r.area()
+r.perimeter()
