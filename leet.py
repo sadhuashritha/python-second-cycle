0b4a0eb98afb0ee077112cd7def1e6578e32df7c
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-04-10 23:46:37 +0530

    updated

--- leet.py
+++ leet.py
@@ -40,3 +40,15 @@ class Developer(Employee):
 d = Developer("Ashritha","Sadhu")
 print(d.name)
 print(d.fullname)
+
+# Count Integers With Even Digit Sum
+class Solution:
+    def countEven(self, num: int) -> int:
+        count = 0
+        for i in range(1,num+1):
+            sumi = 0
+            for j in str(i):
+                sumi += int(j)
+            if sumi % 2 == 0:
+                count+=1
+        return count
