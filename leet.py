9fa99e3f885fe9ee8bfd0aaa4bc05d6b47d352d6
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-04-17 22:22:31 +0530

    updated

--- leet.py
+++ leet.py
@@ -120,3 +120,12 @@ class Solution:
                     break
         return prices
 
+# 2670. Find the Distinct Difference Array
+class Solution:
+    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        arr = []
+        for i in range(len(nums)):
+            prefix = set(nums[:i+1])
+            suffix = set(nums[i+1:])
+            arr.append(len(prefix) - len(suffix))
+        return arr
