4a5e542fe4b23b47730eb55514915c0ff4f03046
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-04-20 22:36:13 +0530

    updated

--- leet.py
+++ leet.py
@@ -175,11 +175,3 @@ class Solution:
         if x % sum == 0:
             return sum
         return -1
-# Array Partition
-class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        sum = 0
-        for i in range(0,len(nums),2):
-            sum += nums[i]
-        return sum
