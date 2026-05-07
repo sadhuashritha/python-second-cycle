0e3e1f2e21f7aee24d27601bdeae575ad3017d95
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-05-07 22:49:25 +0530

    Updated

--- leet.py
+++ leet.py
@@ -177,3 +177,11 @@ class Solution:
         return -1
 
 
+# Array Partition
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        sum = 0
+        for i in range(0,len(nums),2):
+            sum += nums[i]
+        return sum
