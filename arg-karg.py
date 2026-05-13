c185d92847d4d1ce183a4f634dff0b219c54f44d
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-05-13 13:54:44 +0530

    updated

--- arg-karg.py
+++ arg-karg.py
@@ -14,3 +14,30 @@ def add(*n):
 add(1,2,3)
 add(1,2,3,4,5,6,7,8,9,0)
 add(3,2,5,7)
+print() 
+
+# Write a function using **kwargs to print student details.
+def student(**n):
+    print(type(n["name"]))
+    print(n["age"])
+    print(n)
+student(name = "ashritha",age = 20,section = "Beta")
+print()
+
+# Create a function using **kwargs to display employee information.
+def student(**n):
+    print(type(n["name"]))
+    print(n["age"])
+    print(n)
+student(name = "ashritha",age = 20,department = "HR")
+print()
+
+# Write a function using *args to calculate average.
+def avg(*n):
+    ans = sum(n) / len(n)
+    print(ans)
+avg(2,3,1,4,5)
+avg(1,2,3)
+avg(0,6,7,8)
+print()
+
