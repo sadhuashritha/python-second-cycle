afad766d9ffe51f132bc35941d99bb7a58c8d43c
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-05-15 21:32:32 +0530

    updated

---
+++ filehandling.py
@@ -0,0 +1,14 @@
+# n = open("fhandling.py","w")
+# n.write("Filehandling")
+# n.close()
+
+# n = open("fhandling.py","r")
+# print(n.read())
+# n.close()
+
+# n = open("fhandling.py","a")
+# n.write("Hello World")
+
+n = open("fhandling1.py","x")
+n.write("Hello")
+n.close
