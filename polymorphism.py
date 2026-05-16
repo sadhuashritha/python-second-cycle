69c2fd3ca2ac6976728826cbfcad9d4bc62e4e48
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-05-16 20:04:21 +0530

    updated

--- polymorphism.py
+++ polymorphism.py
@@ -8,6 +8,7 @@ class Child(Parent):
         print("Child class")
 c = Child()
 c.details()
+print()
 # Create operator overloading example.
 class operator:
     def __init__(self,a):
@@ -17,9 +18,36 @@ class operator:
 o = operator(5)
 o1 = operator(10)
 print(o+o1)
+print()
+
 # Create polymorphism using inheritance.
+class Parent:
+    def details(self):
+        print("Parent class")
+class Child(Parent):
+    def details(self):
+        print("Child class")
+c = Child()
+c.details()
+print()
 # Create a program where different classes use same method name.
+class Parent:
+    def details(self):
+        print("Parent class")
+class Child(Parent):
+    def details(self):
+        print("Child class")
+c = Child()
+c.details()
+print()
 # Create shape area calculation using polymorphism.
+class Area:
+    def rect(self,x=1,y=1):
+        return 2 * (x + y)
+a = Area()
+print(a.rect(2,3))
+print(a.rect())
+
 # Create animal sound program using polymorphism.
 # Create polymorphism using loops.
 # Create duck typing example.
