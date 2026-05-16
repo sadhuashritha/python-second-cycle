bbdfb045b0f73c87d27ee168de577c0833050a00
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-05-16 09:34:55 +0530

    updated

--- method.py
+++ method.py
@@ -21,7 +21,34 @@ add = addition(1,2)
 print(add.res())
 print()
 
+class addition:
+    def __init__(self,value):
+        self.value = value
+    def __add__(a,b):
+        return a.value + b.value
+x = addition(3)
+y = addition(3)
+print(x + y)
+print()
+
 # Create a method returning square of a number.
+class Square:
+    def __init__(self,a):
+        self.a = a
+    def res(self):
+        print(self.a * self.a)
+s = Square(8)
+s.res()
+print()
+
+class Square:
+    def __init__(self,value):
+        self.value = value
+    def __pow__(a,b):
+        return a.value ** b.value
+x = Square(4)
+y = Square(2)
+print(x ** y)
 # Create a method to update employee salary.
 # Create a class method using @classmethod.
 # Create a static method using @staticmethod.
