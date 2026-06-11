c4d84c67032fa64d838ea6d17b8f62b673f75cee
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-06-11 22:43:54 +0530

    updated

--- bubblesort.py
+++ bubblesort.py
@@ -1,3 +1,4 @@
+
 arr = [5,3,1,4]
 n = len(arr)
 for i in range(n): # 0
@@ -18,4 +19,20 @@ for i in range(n): # 0
 # | 1        | 1 | 2   | (1,2)            |
 # | 2        | 0 | 1   | (0,1)            |
 # -----------------------------------------
+print()
+
+# Input: nums = [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
+nums = [2,0,2,1,1,0]
+n = len(nums)
+for i in range(n):
+    for j in range(n-i-1):
+        if nums[j] > nums[j+1]:
+            temp = nums[j] 
+            nums[j] = nums[j+1]
+            nums[j+1] = temp
+print(nums)
+print()
+print()
+print()
 
