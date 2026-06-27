0ba1051b14fff9a165ac11750262b5f10b17b8dd
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-06-27 21:00:00 +0530

    Solved LeetCode problems

--- leet.py
+++ leet.py
@@ -216,3 +216,16 @@ class Solution:
             return max(ele)
         else:
             return -1
+
+# Student Attendance Record I
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        count = 0
+        for i in range(len(s)):
+            if s[i] == "A":
+                count += 1
+        if count < 2 and "LLL" not in s:
+            return True
+        return False
+
+
