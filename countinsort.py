a9560821119f88b453fd8336a0ec2fcac593ae7d
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-07-08 18:30:11 +0530

    updated

--- countinsort.py
+++ countinsort.py
@@ -1,4 +1,23 @@
-# Stable sorting:
+# Counting sort
+def countingsort(arr):
+    s = max(arr)
+    count = [0] * (s + 1)
+    for i in arr:
+        count[i] += 1
+    j = 0
+    for i in range(len(count)):
+        while count[i] > 0:
+            arr[j] = i
+            j += 1
+            count[i] -= 1
+    return arr
+arr = [4,3,2,2,6,2,8,4,3,0]
+print(countingsort(arr))
+
+print()
+
+
+# counting Stable sorting:
 def countingsort(arr):
     s = max(arr)
     count = [0] * (s + 1)
@@ -13,5 +32,5 @@ def countingsort(arr):
         ans[count[arr[i]]-1] = arr[i]
         count[arr[i]] -= 1
     return ans
-arr = [4,3,2,5,6,1,8,4,3,0]
+arr = [4,3,2,2,6,2,8,4,3,0]
 print(countingsort(arr))
