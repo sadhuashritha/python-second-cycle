6058a57d01481e4d2fe4d6076446851a63485ccb
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-07-16 21:00:00 +0530

    Solved LeetCode problems

--- leet.py
+++ leet.py
@@ -321,3 +321,23 @@ class Solution:
         for i in range(len(prefix)):
             ans[i] = prefix[i] * suffix[i]
         return ans
+    
+# 1679. Max Number of K-Sum Pairs
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        count = 0
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            if nums[left] + nums[right] == k:
+                count += 1
+                left += 1
+                right -= 1
+            elif nums[left] + nums[right] < k:
+                left += 1
+            else:
+                right -= 1
+        return count
+                
