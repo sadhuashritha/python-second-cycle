db7261a6eccda751c03783c9e31dd670c9270735
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-07-18 21:00:00 +0530

    Solved LeetCode problems

--- leet.py
+++ leet.py
@@ -342,3 +342,12 @@ class Solution:
         return count
 
 # 5. Longest Palindromic Substring
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longsub = ""
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                sub = s[i:j+1]
+                if sub == sub[::-1] and len(longsub) < len(sub):
+                    longsub = sub
+        return longsub 
