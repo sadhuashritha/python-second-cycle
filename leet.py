679c9197d307675d894ae5409430c140644c36f0
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-07-20 21:00:00 +0530

    Solved LeetCode problems

--- leet.py
+++ leet.py
@@ -352,12 +352,4 @@ class Solution:
                     longsub = sub
         return longsub 
 
-# 2037. Minimum Number of Moves to Seat Everyone
-class Solution:
-    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        seats.sort()
-        students.sort()
-        count = 0
-        for i,j in zip(seats,students):
-            count += abs(i-j)
-        return count
+
