1240acb53a8bf9b68aa44564f30c2e4bb25d85e4
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-07-20 23:08:41 +0530

    updated

--- sliding.py
+++ sliding.py
@@ -16,8 +16,22 @@ def sliding(arr,k):
 arr = ['a','i','o','r','t','u','o']
 k = 4
 print(sliding(arr,k))
+
 # arr1 = ['b','e','a','t','i','o','n']
 # k1 = 3
 # print(sliding(arr1,k1))
+
+# 643. Maximum Average Subarray I
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        total_sum = sum(nums[:k]) 
+        # total_avg = total_sum / k
+        max_avg =  total_sum
+        for i in range(k,len(nums)):
+            total_sum += nums[i] - nums[i - k] 
+            # total_avg = total_sum / k
+            max_avg = max(max_avg,total_sum)
+        return max_avg / k
+              
 
 
