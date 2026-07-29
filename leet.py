f8946866fba66550715050f98a11323e8da30d78
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-07-29 22:39:48 +0530

    updated

--- leet.py
+++ leet.py
@@ -381,9 +381,3 @@ class Solution:
         return res
     
 
-# 1464. Maximum Product of Two Elements in an Array
-class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        nums.sort(reverse= True)
-        ans = (nums[0]-1)*(nums[1]-1)
-        return ans
